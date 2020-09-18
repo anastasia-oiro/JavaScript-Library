49e0385257ba4154b6578d7a3ab94848f2d113e0
anastasia-oiro <anastasiaoiro23@gmail.com>
2020-09-18 14:40:26 +0300

    index.html library.js

---
+++ library.js
@@ -0,0 +1,21 @@
+let myLibrary = [];
+function Book(title,author,pages,read){
+    this.title=title
+    this.author=author
+    this.pages=pages
+    this.read=read
+     this.info=function(){
+        return'title,author,pages,read'
+    } 
+}
+
+
+
+// function addBookToLibrary() {
+  
+// }
+
+document.querySelector('.add-btn')
+.addEventListener('click' ,function(evt){
+
+})
