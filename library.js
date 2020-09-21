f44bf5a2aa892fa693206e79100fc7362c5f8480
anastasia-oiro <anastasiaoiro23@gmail.com>
2020-09-21 06:50:42 +0300

    JavaScript-Library

--- library.js
+++ library.js
@@ -1,21 +1,30 @@
+const titleInput= document.querySelector("#title");
+const authorInput=document.querySelector("#author");
+const pagesInput=document.querySelector("#pages");
+const isReadInput=document.querySelector("#yes");
+const addButton=document.querySelector("#done");
+
+addButton.addEventListener('click',addBookToLibrary);
+
 let myLibrary = [];
-function Book(title,author,pages,read){
+let id =0;
+
+function Book(title,author,pages,isRead){ 
     this.title=title
     this.author=author
     this.pages=pages
-    this.read=read
-     this.info=function(){
-        return'title,author,pages,read'
-    } 
-}
+    this.isRead=isRead
+     }
+ function addBookToLibrary() {
+  if (titleInput.value=="" && authorInput.value=="" && pagesInput.value=="") {
+    alert("Enter all fields") ; 
+  }
+  else{
+    const newEntry = new Book(titleInput.value,authorInput.value,pagesInput.value,isReadInput.value)
+    myLibrary.push(newEntry);
+    console.log(myLibrary)
+    document.querySelector('#form').reset(); //clears form for the next entry
 
+  }
+ }
 
-
-// function addBookToLibrary() {
-  
-// }
-
-document.querySelector('.add-btn')
-.addEventListener('click' ,function(evt){
-
-})
